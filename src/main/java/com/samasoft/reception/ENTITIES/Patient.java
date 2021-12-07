dc93dd40eb220ddeb8ba97663e836ade5633594f
augum <augumakuma@gmail.com>
2021-12-07 12:19:18 +0100

    Creation de la methode post pour categorie

--- src/main/java/com/samasoft/reception/ENTITIES/Patient.java
+++ src/main/java/com/samasoft/reception/ENTITIES/Patient.java
@@ -18,7 +18,7 @@ public class Patient {
     private String prenom;
     private String etatCivil;
     private String sexe;
-    @OneToMany
+    @OneToOne
     private Nationalite nationalite;
     private String avenue;
     private String quartier;
@@ -34,6 +34,6 @@ public class Patient {
     private Categorie categorie;
     private String titulaire;
     private String matricule;
-    @OneToMany
+    @OneToOne
     private Profession profession;
 }
--- src/main/java/com/samasoft/reception/WEBAPI/CategorieWebApi.java
+++ src/main/java/com/samasoft/reception/WEBAPI/CategorieWebApi.java
@@ -1,9 +1,29 @@
 package com.samasoft.reception.WEBAPI;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.samasoft.reception.DTO.CategorieRequestDTO;
+import com.samasoft.reception.DTO.CategorieResponseDTO;
+import com.samasoft.reception.SERVICES.Categorie.CategorieService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "/api")
+@AllArgsConstructor
 public class CategorieWebApi {
+    private CategorieService categorieService;
+     // enregistrement d'une nouvelle categorie
+    @PostMapping(path = "/categories/create")
+    public CategorieResponseDTO save(@RequestBody CategorieRequestDTO requestDTO){
+        //attribution de l'ID
+        requestDTO.setId(UUID.randomUUID().toString());
+        return  categorieService.save(requestDTO);
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> exceptionHandler(Exception e){
+        return  new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
