d1417895dfe94b6d6be2e87c15e082dd10ed4427
augum <augumakuma@gmail.com>
2021-12-08 10:42:52 +0100

    fin developpement categorie du service reception

--- src/main/java/com/samasoft/reception/WEBAPI/CategorieWebApi.java
+++ src/main/java/com/samasoft/reception/WEBAPI/CategorieWebApi.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -16,12 +17,31 @@ import java.util.UUID;
 public class CategorieWebApi {
     private CategorieService categorieService;
      // enregistrement d'une nouvelle categorie
-    @PostMapping(path = "/categories/create")
+    @PostMapping(path = "/categories")
     public CategorieResponseDTO save(@RequestBody CategorieRequestDTO requestDTO){
         //attribution de l'ID
         requestDTO.setId(UUID.randomUUID().toString());
         return  categorieService.save(requestDTO);
     }
+    @PutMapping(path = "/categories/{id}")
+    public CategorieResponseDTO update(@PathVariable String id,@RequestBody CategorieRequestDTO categorieRequestDTO){
+        return categorieService.update(id,categorieRequestDTO);
+    }
+    @DeleteMapping(path = "/categories/{id}")
+    public void delete(@PathVariable String id){
+        categorieService.delete(id);
+    }
+    @GetMapping(path = "/categories/{id}")
+
+    public CategorieResponseDTO getArticle(@PathVariable String id){
+        return categorieService.getCategorie(id);
+    }
+    @GetMapping(path = "/categories")
+    public List<CategorieResponseDTO> getListArticle(){
+        return categorieService.listeCategorie();
+    }
+    
+    //pour gerer les exceptions métiers
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> exceptionHandler(Exception e){
         return  new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
